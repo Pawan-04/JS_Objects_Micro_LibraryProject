cb3229a0847e1622c7faa09aa97dbc96c28e58bb
Pawan Kumar Pandey <pawan4kp@gmail.com>
2025-08-19 19:32:19 +0530

    project Completed

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 class Library{
     constructor(){
-        this.book = [] //all books stores here
+        this.books = [] //all books stores here
     }
 
-    addBooks(book){
-        this.book.push(book)
+    addBooks(b){
+        this.books.push(...b)
     }
 
     listBooks(){
-        this.book.forEach((b)=>{
-            console.log(b)
+        this.books.forEach((elem)=>{
+            console.log(elem.name)
         })
     }
 }
 
-let BennettLibrary = new Library();
-BennettLibrary.addBooks("Mera AAjevan Karavas")
-BennettLibrary.listBooks()
-console.log(BennettLibrary)
+
+class Book{
+    constructor(name,isbn,price,author){
+        this.name = name;
+        this.price = price;
+        this.isbn = isbn;
+        this.author = author
+        this.readStatus = false
+    }
+
+    info(){
+        console.log(`${this.name} book is available on amazon at price ${this.price} and it was written by ${this.author} ${this.readStatus ? "🟢You have read this book" : "🔴You haven't read yet thius book"} `)
+    } 
+
+    changeStatus(){
+        this.readStatus = !this.readStatus
+    }
+
+}
+let b1 = new Book("ram teri ganaga maili", '#7abcd52', 250, "Raveena singh")
+let b2 = new Book("ganaga", '#88bcd52', 50, "Chandra Shekharsingh")
+
+
+let BennettLibrary = new Library()
+BennettLibrary.addBooks([b1,b2])
